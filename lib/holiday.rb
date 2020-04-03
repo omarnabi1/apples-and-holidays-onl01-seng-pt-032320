cf4eddc3af3f10e54362a94f39bacd5dfbf508da
Omar Nabi <onabi20@gmail.com>
2020-04-03 06:42:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+holiday_hash.each do |season,holiday|
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |holiday_event, supply|
+      puts "  #{holiday_event.to_s.split("_").each{|element| element.capitalize!}.join(" ")}: #{supply.join(", ")}" 
+    end
+  end  
 end
 
 def all_holidays_with_bbq(holiday_hash)
